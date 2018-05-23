7f22bc221c0d14e5b9b5b673f9070d98d378ad7c
Lauren Abrams <lauren.abrams@powerhrg.com>
2018-05-23 12:18:55 -0400

    finished anagram detector lab

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
-# Your code goes here!
+require'pry'
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(anagram_array)
+   match_array = []
+   anagram_array.each do |anagram|
+    if anagram.split("").sort == word.split("").sort
+      match_array << anagram
+    end
+  end
+  match_array
+end
+end
